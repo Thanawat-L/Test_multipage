2da3dbd2250d8a525e7e268000e7ee8753e2ed80
Thanawat-L <158482818+Thanawat-L@users.noreply.github.com>
2024-11-06 15:34:05 +0000

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is ahead of 'origin/main' by 1 commit. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   streamlit_app.py #

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from home import home_page
 from page1 import page1_page
 from page2 import page2_page
 
-st.sidebar.title("Navigation")
+st.sidebar.title("เลือกหน้า")
 page = st.sidebar.radio("Go to", ["Home", "Page 1", "Page 2"])
 
 if page == "Home":
